2060f3940f807201ea4a628c757a0c4fd85a65cb
JimmyN025 <76139887+JimmyN025@users.noreply.github.com>
2021-07-08 09:56:53 +0700

    upload project

---
+++ app/src/main/java/ac/id/atmaluhur/mahasiswa/aplikasilokasi/api/ApiClient.java
@@ -0,0 +1,27 @@
+package ac.id.atmaluhur.mahasiswa.aplikasilokasi.api;
+
+
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    public static final String URL = "http://192.168.100.8/jim/";
+    public static Retrofit RETROFIT = null;
+
+    public static Retrofit getClient(){
+        if (RETROFIT==null){
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor((Interceptor) new LoggingInterceptor())
+                    .build();
+            RETROFIT = new Retrofit.Builder()
+                    .baseUrl(URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return RETROFIT;
+    }
+}
